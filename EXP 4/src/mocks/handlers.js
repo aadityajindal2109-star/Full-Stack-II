180e21efef994ef27fd739ba20143d72ddc5b2bf
AADITYA JINDAL <aadityajindal@AADITYAs-MacBook-Air.local>
2026-09-18 13:57:23 +0530

    Remove Experiment 4

--- EXP 4/src/mocks/handlers.js
+++
@@ -1,7 +0,0 @@
-import { http, HttpResponse } from "msw";
-
-export const handlers = [
-  http.get("/api/events", () => {
-    return HttpResponse.json([{ id: 1, title: "Mock Event", date: "2026-09-07" }]);
-  }),
-];
